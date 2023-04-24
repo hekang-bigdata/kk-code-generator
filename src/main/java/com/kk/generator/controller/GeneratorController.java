81ed4760dfbf89d1f3a6e48b255ac913766f6e82
hk <hekang_14@163.com>
2023-04-24 10:26:55 +0800

    获得数据库数据库中的表 以及表的字段属性

---
+++ src/main/java/com/kk/generator/controller/GeneratorController.java
@@ -0,0 +1,12 @@
+package com.kk.generator.controller;
+
+/**
+ * @ProjectName: kk-code-generator
+ * @Package: com.kk.generator.controller
+ * @ClassName: Generator
+ * @Author: HK
+ * @Description:
+ * @Date: 2023/4/23 23:38
+ * @Version: 1.0
+ */
+public class GeneratorController {}
